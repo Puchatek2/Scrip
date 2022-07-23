1567439f06bd6780fba6c73de089827f4a975cb5
Puchatek2 <100354227+Puchatek2@users.noreply.github.com>
2022-07-23 13:24:27 +0000

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,9 @@
+local HWIDTable = loadstring(game:HttpGet("https://pastebin.com/raw/UkixRfi9"))()
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+
+for i,v in pairs(HWIDTable) do
+    if v == HWID then
+        print ("Hardware ID Passed, You can load the script now...")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Puchatek2/asdasd/main/Script-hub.lua"))()
+    end
+end
